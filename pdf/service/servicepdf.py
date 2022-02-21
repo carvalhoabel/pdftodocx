2f6b0dccc66a4bdd2cec31095bb4acfb4b486a1f
abelbcarvalho <abelbcarvalho@gmail.com>
2022-02-21 10:18:32 -0300

    service pdf checker

--- pdf/service/servicepdf.py
+++ pdf/service/servicepdf.py
@@ -1,4 +1,6 @@
 from pdf.model.pdf import Pdf
+from core.singleton.singmessage import SingMessage as Msg
+from core.tools.tools import isitdir, isitfile
 
 
 class ServicePdf:
@@ -22,4 +24,33 @@ class ServicePdf:
         Returns:
             bool: True if converted, False else.
         """
-        pass
+        if not pdf.pathpdf:
+            Msg.message().cleardata()
+            Msg.message().message = 'Warning! Pdf path Empty'
+            Msg.message().title = 'Warning - Pdf Path'
+            Msg.message().warning = True
+            return False
+        if not pdf.pathdocx:
+            Msg.message().cleardata()
+            Msg.message().message = 'Warning! Docx path save empty'
+            Msg.message().title = 'Warning - Docx path'
+            Msg.message().warning = True
+            return False
+        if not isitfile(pathfile=pdf.pathpdf):
+            Msg.message().cleardata()
+            Msg.message().message = 'Warning! Pdf not exists'
+            Msg.message().title = 'Warning - Pdf File'
+            Msg.message().warning = True
+            return False
+        if not isitdir(path=pdf.pathdocx):
+            Msg.message().cleardata()
+            Msg.message().message = 'Warning! Docx saving invalid path'
+            Msg.message().title = 'Warning - Docx File'
+            Msg.message().warning = True
+            return False
+        if not pdf.name:
+            name = list(pdf.pathpdf)
+            name.reverse()
+            name = name[:name.index('/')]
+            name.reverse()
+            pdf.name = ''.join(name)
