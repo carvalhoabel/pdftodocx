dcd671288a5f2ca57cf2a7a046eff0986f92d014
abelbcarvalho <abelbcarvalho@gmail.com>
2022-02-21 10:31:50 -0300

    service checker and link to conversor

---
+++ core/conversor/conversor.py
@@ -0,0 +1,26 @@
+from pdf.model.pdf import Pdf
+
+
+class Conversor:
+    """Conversor class back-end, when it takes
+    from pdf file and transform it to docx.
+    
+    Author:
+        abelbcarvalho
+    """
+    
+    def __init__(self) -> None:
+        """New Conversor.
+        """
+        pass
+    
+    def convert_pdf_to_docx(self, pdf: Pdf) -> bool:
+        """Converts from pdf to docx.
+
+        Args:
+            pdf (Pdf): pdf file.
+
+        Returns:
+            bool: True if converted, False else.
+        """
+        pass
--- pdf/service/servicepdf.py
+++ pdf/service/servicepdf.py
@@ -1,6 +1,7 @@
 from pdf.model.pdf import Pdf
 from core.singleton.singmessage import SingMessage as Msg
 from core.tools.tools import isitdir, isitfile
+from core.conversor.conversor import Conversor
 
 
 class ServicePdf:
@@ -13,7 +14,11 @@ class ServicePdf:
     def __init__(self) -> None:
         """New Pdf Service.
         """
-        pass
+        self._conversor = Conversor()
+
+    @property
+    def conversor(self) -> Conversor:
+        return self._conversor
 
     def convert_pdf_to_docx(self, pdf: Pdf) -> bool:
         """Converts from pdf to docx.
@@ -54,3 +59,20 @@ class ServicePdf:
             name = name[:name.index('/')]
             name.reverse()
             pdf.name = ''.join(name)
+        if pdf.name.__len__() < 2:
+            pdf.name = '/filedocx.docx'
+        elif pdf.name[0] != '/':
+            pdf.name = f'/{pdf.name}'
+        # checking success
+        if self.conversor.convert_pdf_to_docx(pdf=pdf):
+            Msg.message().cleardata()
+            Msg.message().message = 'Sucess! PDF was changed to Docx'
+            Msg.message().title = 'Success - PDF Convertort'
+            Msg.message().info = True
+            return True
+        else:
+            Msg.message().cleardata()
+            Msg.message().message = 'Error! PDF wasn\'t changed to Docx'
+            Msg.message().title = 'Error - PDF Convertort'
+            Msg.message().error = True
+            return False
