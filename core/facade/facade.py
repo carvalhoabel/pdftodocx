39f56ad31dc375847bd87fa8b623d491b6cd6bbd
abelbcarvalho <abelbcarvalho@gmail.com>
2022-02-21 10:02:28 -0300

    service implemented in facade

--- core/facade/facade.py
+++ core/facade/facade.py
@@ -1,3 +1,7 @@
+from pdf.model.pdf import Pdf
+from pdf.service.servicepdf import ServicePdf
+
+
 class Facade:
     """Facade Project Class.
 
@@ -5,7 +9,21 @@ class Facade:
         abelbcarvalho
     """
 
+    # access service
+    _serv_pdf = ServicePdf()
+
     def __init__(self) -> None:
         """New Facade.
         """
         pass
+
+    def convert_pdf_to_docx(self, pdf: Pdf) -> bool:
+        """Converts from pdf to docx.
+
+        Args:
+            pdf (Pdf): pdf file.
+
+        Returns:
+            bool: True if converted, False else.
+        """
+        return self._serv_pdf.convert_pdf_to_docx(pdf=pdf)
