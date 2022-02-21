cf703e10fb2fcf806310bd3996f4eaa08f8e02e0
abelbcarvalho <abelbcarvalho@gmail.com>
2022-02-21 09:59:37 -0300

    pdf service

---
+++ pdf/service/servicepdf.py
@@ -0,0 +1,25 @@
+from pdf.model.pdf import Pdf
+
+
+class ServicePdf:
+    """Service checks from model pdf.
+
+    Author:
+        abelbcarvalho    
+    """
+
+    def __init__(self) -> None:
+        """New Pdf Service.
+        """
+        pass
+
+    def convert_pdf_to_docx(self, pdf: Pdf) -> bool:
+        """Converts from pdf to docx.
+
+        Args:
+            pdf (Pdf): pdf file.
+
+        Returns:
+            bool: True if converted, False else.
+        """
+        pass
